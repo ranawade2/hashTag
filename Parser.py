63be756aac756a2bdd17e0db300fea552500c81e
Sameer Ranawade <ranawade2@gmail.com>
2014-09-18 13:22:59 +0530

    modified:   Parser.py

--- Parser.py
+++ Parser.py
@@ -5,8 +5,9 @@
 
 #***************************************#
 
-def parse(tags_string):
+def parse_data_tags(data, tags_string):
 
+	# data = userid
 	# tags_string = "#tag1 #tag2 #tag3 #tag4"
 
 	tags = tags_string.split(" ")
@@ -18,11 +19,34 @@ def parse(tags_string):
 	# for CSV
 	# tags_csv = ",".join(tags)
 
+	print tags
+
+	return data, tags
+
+#***************************************#
+
+def parse_tags(data_tags_string):
+
+	# data_tags_string = "Hello World #tag1 #tag2 #tag3 #tag4 Welcome Harish"
+
+	tags = data_tags_string.split(" ")
+
+	tags = [tag.split("#")[1] for tag in tags if tag[0] == "#"]
+
+	tags.sort()	
+
+	# for CSV
+	# tags_csv = ",".join(tags)
+
+	print tags
+
 	return tags
 
 #***************************************#
 
-parse("#ase #face #slac #blr")
+parse_data_tags("ranawade2", "#java #cpp #py #php")
+
+parse_tags("Hello World #ase #face #slac #blr Welcome Harish")
 
 
 
